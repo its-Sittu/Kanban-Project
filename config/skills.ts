6aa49be4455b8f0e5ddaa94f4895f60370e5dd3a
unknown <sitturaj730@gmail.com>
2026-05-04 23:32:16 +0530

    feat: add GCP to skills ecosystem

--- config/skills.ts
+++ config/skills.ts
@@ -62,4 +62,5 @@ export const skillCategories: SkillCategory[] = [
             { name: "Kubernetes", icon: "code" },
             { name: "AWS", icon: "code" },
             { name: "Azure", icon: "code" },
+            { name: "GCP", icon: "code" },
 ]
