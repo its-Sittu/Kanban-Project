c8d31b68317fb49ad92f51fb061aa1fc82c06c1d
unknown <sitturaj730@gmail.com>
2026-05-04 23:35:29 +0530

    feat: add DatoCMS to skills ecosystem

--- config/skills.ts
+++ config/skills.ts
@@ -124,4 +124,5 @@ export const skillCategories: SkillCategory[] = [
             { name: "Adobe Experience Manager", icon: "code" },
             { name: "Prismic", icon: "code" },
             { name: "Sanity", icon: "code" },
+            { name: "DatoCMS", icon: "code" },
 ]
