568f19775d35bbbac1e0529cf9e634c9de85d0bb
unknown <sitturaj730@gmail.com>
2026-05-04 23:57:37 +0530

    Cleanup: Removing old project files to start fresh

--- app/achievements/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AchievementsSection } from "@/components/sections/achievements"
-
-export default function AchievementsPage() {
-    return (
-        <main className="min-h-screen pt-24 pb-20">
-            <AchievementsSection />
-        </main>
-    )
-}
