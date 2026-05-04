48ed6adb1f5bd5719e47084919f4192982118aca
unknown <sitturaj730@gmail.com>
2026-05-04 23:44:03 +0530

    chore: cleanup portfolio - remove Linode (restoring genuine skills)

--- config/skills.ts
+++ config/skills.ts
@@ -79,5 +79,4 @@ export const skillCategories: SkillCategory[] = [
             { name: "Vercel", icon: "code" },
             { name: "Heroku", icon: "code" },
             { name: "DigitalOcean", icon: "code" },
-            { name: "Linode", icon: "code" },
 ]
